f2084d946ab44f935e9a79077b92a9b8ebee3640
aospiridonov <aospiridonov@gmail.com>
2022-07-25 21:27:34 +0300

    add content to pages

--- lib/screens/color_explore_srceen.dart
+++ lib/screens/color_explore_srceen.dart
@@ -1,10 +1,57 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_go_router_example/repository/repository.dart';
+import 'package:go_router/go_router.dart';
 
 class ColorExploreScreen extends StatelessWidget {
   const ColorExploreScreen({Key? key}) : super(key: key);
 
+  void _onTap(BuildContext context, int index) {
+    GoRouter.of(context).push('/colors/$index');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final repo = context.read<ColorRepository>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          context.findAncestorWidgetOfExactType<MaterialApp>()!.title,
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: kIsWeb ? false : true,
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 250),
+        itemCount: repo.colorMap?.length,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () => _onTap(context, index),
+          child: Container(
+            height: 200,
+            width: 200,
+            color: repo.getColorAt(index),
+            alignment: Alignment.center,
+            child: _DescriptionWidget(index: index),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _DescriptionWidget extends StatelessWidget {
+  const _DescriptionWidget({Key? key, required this.index}) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final repo = context.read<ColorRepository>();
+    return Text(
+      repo.getColorKeyAt(index),
+      style: TextStyle(color: repo.computeTextColorFor(index)),
+    );
   }
 }
--- lib/screens/color_shade_screen.dart
+++ lib/screens/color_shade_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_go_router_example/repository/color_repository.dart';
+import 'package:go_router/go_router.dart';
 
 class ColorShadeScreen extends StatelessWidget {
   const ColorShadeScreen({
@@ -10,8 +14,67 @@ class ColorShadeScreen extends StatelessWidget {
   final int index;
   final int alpha;
 
+  void _onIncreaseAlpha(BuildContext context) => Router.neglect(
+      context,
+      () => context
+          .go('/colors/$index?alpha=${alpha + 10 >= 255 ? 255 : alpha + 10}'));
+  void _onDecreaseAlpha(BuildContext context) => Router.neglect(
+      context,
+      () => context
+          .go('/colors/$index?alpha=${alpha - 10 <= 0 ? 0 : alpha - 10}'));
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final color = context.read<ColorRepository>().getColorAt(index);
+    final name = context.read<ColorRepository>().getColorKeyAt(index);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Color - $name',
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: kIsWeb ? false : true,
+      ),
+      body: ListView.builder(
+        itemCount: 9,
+        itemBuilder: (context, index) {
+          final shade = (index + 1) * 100;
+          return SizedBox(
+            height: 70,
+            child: ListTile(
+              tileColor: color[shade]!.withAlpha(alpha),
+              title: Text(
+                'This is $name with shade $shade',
+                style: const TextStyle(fontWeight: FontWeight.w600),
+              ),
+              subtitle: Text('alpha value: $alpha'),
+              textColor: color[shade]!.computeLuminance() > 0.3
+                  ? Colors.black
+                  : Colors.white,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton.extended(
+            heroTag: 'increase-alpha-fab',
+            onPressed: () => _onIncreaseAlpha(context),
+            label: const Text('increase alpha'),
+            icon: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          FloatingActionButton.extended(
+            heroTag: 'decrease-alpha-fab',
+            onPressed: () => _onDecreaseAlpha(context),
+            label: const Text('decrease alpha'),
+            icon: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
   }
 }
